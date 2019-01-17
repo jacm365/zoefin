a5cd99aad2c1bd8cf34ec7a47d784e2b3c2a8316
Jaime Clavijo <jacm365@gmail.com>
2019-01-17 10:49:52 -0500

    Laravel + migrations

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('profession_id')->references('id')->on('professions');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->smallInteger('age');
+            $table->string('gender');
+            $table->string('zip_code');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
